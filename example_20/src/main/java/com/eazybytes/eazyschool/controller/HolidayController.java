8a1387c303d632d30dc8aa8a6dc35291fe455728
Your Namem <vibollong@gmail.com>
2025-05-19 21:19:41 +0700

    finished Validation

--- example_20/src/main/java/com/eazybytes/eazyschool/controller/HolidayController.java
+++ example_20/src/main/java/com/eazybytes/eazyschool/controller/HolidayController.java
@@ -6,7 +6,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.eazybytes.eazyschool.model.Holiday;
 
@@ -15,8 +17,53 @@ import com.eazybytes.eazyschool.model.Holiday;
 @Controller
 public class HolidayController {
 	
+	/*
 	@RequestMapping("/holidays")
-	public String displayHoliday(Model model) {
+	public String displayHoliday(@RequestParam(required = false, name="festival") boolean festival,
+								@RequestParam(required= false) boolean federal,Model model) {
+		model.addAttribute("festival",festival);
+		model.addAttribute("federal", federal);
+		
+		List<Holiday> holidays = Arrays.asList(
+				new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
+                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
+                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
+                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
+                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
+                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
+				);
+		// We want to get the value Festival or Federal 
+		Holiday.Type[] types = Holiday.Type.values();
+		
+		for(Holiday.Type type : types) {
+			model.addAttribute(type.toString(), 
+					holidays.stream().filter(holiday -> holiday.getType().equals(type))
+					.collect(Collectors.toList())
+					);			
+		}
+		
+		return "holidays.html";
+	}
+	
+	*/
+	
+	@RequestMapping("/holidays/{display}")
+	public String displayHoliday(@PathVariable String display,Model model) {
+		
+		if(display !=null && display.equals("all")) {
+			model.addAttribute("festival",true);
+			model.addAttribute("federal", true);
+		}else if(display !=null && display.equals("festival")) {
+			model.addAttribute("festival",true);
+			model.addAttribute("federal", false);
+		}else if(display !=null && display.equals("federal")) {
+			model.addAttribute("festival",false);
+			model.addAttribute("federal", true);
+		}
+		
+		
 		List<Holiday> holidays = Arrays.asList(
 				new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
                 new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
