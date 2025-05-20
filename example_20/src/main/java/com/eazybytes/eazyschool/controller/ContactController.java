391834c3679fad474f41cb38f73093612a962be2
longvibol <vibollong@gmail.com>
2025-05-20 16:12:51 +0700

    erwer

--- example_20/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ example_20/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -1,15 +1,16 @@
 package com.eazybytes.eazyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.service.ContactService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,38 +18,65 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequiredArgsConstructor
 public class ContactController {
-	
+
 	private final ContactService contactService;
-	
-	
-	
-	 @RequestMapping("/contact")
-	    public String displayContactPage() {
-	        return "contact.html";
-	    }
-	 
-	 /*
-//	 @RequestMapping(value="/saveMsg", method = RequestMethod.POST)
-	 @PostMapping("/saveMsg")
-	 public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
-			 @RequestParam String email,@RequestParam String subject, @RequestParam String message) {
-		 
-		 log.info("Name : " + name);
-		 log.info("Mobile Number : " + mobileNum);
-		 log.info("Email Address : " + email);
-		 log.info("Subject : " + subject);
-		 log.info("Message : " + message);
-		 
-		 return new ModelAndView("redirect:/contact");
-	 }
-	 
+
+	@RequestMapping("/contact")
+	public String displayContactPage(Model model) {
+		model.addAttribute("contact", new Contact());
+		return "contact.html";
+	}
+
+	/*
+	 * @RequestMapping("/contact") public String displayContactPage() { return
+	 * "contact.html"; }
 	 */
 
-	 @PostMapping("/saveMsg")
-	 public ModelAndView saveMessage(Contact contact) {
-		 contactService.saveMessageDetails(contact);		 		 
-		 return new ModelAndView("redirect:/contact");
-	 }
-	 
+	/*
+	 * // @RequestMapping(value="/saveMsg", method = RequestMethod.POST)
+	 * 
+	 * @PostMapping("/saveMsg") public ModelAndView saveMessage(@RequestParam String
+	 * name, @RequestParam String mobileNum,
+	 * 
+	 * @RequestParam String email,@RequestParam String subject, @RequestParam String
+	 * message) {
+	 * 
+	 * log.info("Name : " + name); log.info("Mobile Number : " + mobileNum);
+	 * log.info("Email Address : " + email); log.info("Subject : " + subject);
+	 * log.info("Message : " + message);
+	 * 
+	 * return new ModelAndView("redirect:/contact"); }
+	 * 
+	 */
+
+	/*
+	@PostMapping("/saveMsg")
+	public ModelAndView saveMessage(Contact contact) {
+		contactService.saveMessageDetails(contact);
+		return new ModelAndView("redirect:/contact");
+	}
+	
+	*/
+	
+	@PostMapping("/saveMsg")
+	public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+		if(errors.hasErrors()) {
+			log.error("Contact form validation failed due to : "+ errors.toString());
+			return "contact.html";
+		}
+		contactService.saveMessageDetails(contact);
+		contactService.setCounter(contactService.getCounter()+1);
+		log.info("Number of times the Contact form is submited count: " +contactService.getCounter());
+		return "redirect:/contact";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- example_20/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
+++ example_20/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -1,19 +1,32 @@
 package com.eazybytes.eazyschool.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 import com.eazybytes.eazyschool.model.Contact;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+//@RequestScope
+//@SessionScope
+@ApplicationScope
+@Data
 public class ContactService {
+	
+	private int counter=0;
+	
+	public ContactService() {
+		System.out.println("Contact Service Bean initialized");
+	}
 	
 	public boolean saveMessageDetails(Contact contact) {
 		boolean isSaved = true;
-		//TODO need to persist  the data into the DB table 
-		
+		//TODO need to persist  the data into the DB table 		
 		log.info(contact.toString());
 		return isSaved;
 	}
