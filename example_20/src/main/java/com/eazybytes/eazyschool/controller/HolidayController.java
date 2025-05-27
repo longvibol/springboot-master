d6a13f95c1ca9149662c7526667941840228a67a
longvibol <vibollong@gmail.com>
2025-05-27 16:31:29 +0700

    f

--- example_20/src/main/java/com/eazybytes/eazyschool/controller/HolidayController.java
+++ example_20/src/main/java/com/eazybytes/eazyschool/controller/HolidayController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.eazybytes.eazyschool.model.Holiday;
+import com.eazybytes.eazyschool.repository.HolidaysRepository;
 
 
 
 @Controller
 public class HolidayController {
+	
+	@Autowired
+	private HolidaysRepository holidaysRepository;
 	
 	/*
 	@RequestMapping("/holidays")
@@ -64,16 +69,8 @@ public class HolidayController {
 		}
 		
 		
-		List<Holiday> holidays = Arrays.asList(
-				new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
-				);
+		List<Holiday> holidays = holidaysRepository.findAllHolidays();
+			
 		// We want to get the value Festival or Federal 
 		Holiday.Type[] types = Holiday.Type.values();
 		
--- example_20/src/main/java/com/eazybytes/eazyschool/model/Holiday.java
+++ example_20/src/main/java/com/eazybytes/eazyschool/model/Holiday.java
@@ -3,11 +3,17 @@ package com.eazybytes.eazyschool.model;
 import lombok.Data;
 
 @Data
-public class Holiday {
+public class Holiday extends BaseEntity{
 
+	/* We remove the final keyword becasue we want to change from backend and for RowMapper it also need to change
     private final String day;
     private final String reason;
     private final Type type;
+    
+    */
+    private  String day;
+    private  String reason;
+    private  Type type;
 
     public enum Type {
         FESTIVAL, FEDERAL
