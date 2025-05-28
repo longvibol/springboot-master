cb02dd3e18e4be94edb191fe2223bc599421e66c
longvibol <vibollong@gmail.com>
2025-05-28 11:04:56 +0700

    localdatabase

--- example_20/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_20/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,14 +1,15 @@
 package com.eazybytes.eazyschool.config;
-
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -20,14 +21,13 @@ public class ProjectSecurityConfig {
 	        .csrf(csrf -> 
 	        		csrf
 	        		.ignoringRequestMatchers("/saveMsg")
-	        		.ignoringRequestMatchers(PathRequest.toH2Console())) 
+	        		) 
 	        .authorizeHttpRequests(auth -> auth
 	            .requestMatchers("/dashboard").authenticated()
-	            .requestMatchers(PathRequest.toH2Console()).permitAll()
 	            .requestMatchers("/displayMessages").hasRole("ADMIN")
 	            .requestMatchers("/closeMsg/**").hasRole("ADMIN")
 	            .requestMatchers("/", "/home", "/holidays/**", "/contact", "/saveMsg", 
-	                             "/courses", "/about", "/assets/**", "/login", "/logout","/h2-console/**")
+	                             "/courses", "/about", "/assets/**", "/login", "/logout")
 	  
 	            .permitAll()
 	        )
@@ -38,70 +38,34 @@ public class ProjectSecurityConfig {
 	            .permitAll()
 	        )
 	        
-	        .httpBasic(Customizer.withDefaults())
-	    .headers().frameOptions().disable();
+	        .httpBasic(Customizer.withDefaults());
+	    
 
 	    return http.build();
 	}	
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+	
 	//Create temporary user 
 	@Bean
-	public InMemoryUserDetailsManager userDetailsManager() {
-		UserDetails admin = User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("12345")
-				.roles("USER")
-				.build();
-		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("admin")
-				.password("12345")
-				.roles("ADMIN")
-				.build();		
-		return new InMemoryUserDetailsManager(user, admin);				
-				
-	}	
-	
-	/*
-	@Bean
-	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
-//		http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());	
-		http.formLogin(withDefaults());
-		http.httpBasic(withDefaults());
-		return http.build();
+	public InMemoryUserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder) {
+	    UserDetails admin = User.builder()
+	            .username("admin")
+	            .password(passwordEncoder.encode("12345"))
+	            .roles("ADMIN")
+	            .build();
+
+	    UserDetails user = User.builder()
+	            .username("user")
+	            .password(passwordEncoder.encode("12345"))
+	            .roles("USER")
+	            .build();
+
+	    return new InMemoryUserDetailsManager(admin, user);
 	}
-	
-	
-	@Bean
-	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf().ignoringRequestMatchers("/saveMsg").and()
-			.authorizeHttpRequests((requests) -> {
-			try {
-				requests
-						.requestMatchers("/dashboard").authenticated()
-						.requestMatchers("/","/home").permitAll()
-						.requestMatchers("/holidays/**").permitAll()
-						.requestMatchers("/contact").permitAll()
-						.requestMatchers("/saveMsg").permitAll()
-						.requestMatchers("/courses").permitAll()
-						.requestMatchers("/about").permitAll()
-						.requestMatchers("/assets/**").permitAll()	
-						.requestMatchers("/login").permitAll()
-						.requestMatchers("/logout").permitAll()
-						.and().formLogin().loginPage("/login")
-						.defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
-//						.and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll()
-						.and().httpBasic();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
-				
-		return http.build();
-	}
-	*/		
-	
-	
 
 }
 
