373a36f0d7f35cf4a63502a07c55af73dcc2d5e6
longvibol <vibollong@gmail.com>
2025-05-29 08:10:34 +0700

    er

--- example_20/src/main/java/com/eazybytes/eazyschool/repository/ContactRepository.java
+++ example_20/src/main/java/com/eazybytes/eazyschool/repository/ContactRepository.java
@@ -1,17 +1,19 @@
 package com.eazybytes.eazyschool.repository;
 
-import com.eazybytes.eazyschool.model.Contact;
-import com.eazybytes.eazyschool.rommappers.ContactRowMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
-import org.springframework.stereotype.Repository;
-
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.stereotype.Repository;
+
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.rommappers.ContactRowMapper;
 
 /*
 @Repository stereotype annotation is used to add a bean of this class
@@ -19,7 +21,7 @@ type to the Spring context and indicate that given Bean is used to perform
 DB related operations and
 * */
 @Repository
-public class ContactRepository {
+public class ContactRepository{
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -56,5 +58,7 @@ public class ContactRepository {
             }
         });
     }
+     
+    
 
 }
