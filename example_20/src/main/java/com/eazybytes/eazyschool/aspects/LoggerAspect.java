a7413e1ae6c96e3b2ee6df5fa78d1dcbce9bb51e
longvibol <vibollong@gmail.com>
2025-07-10 10:46:53 +0700

    findh

--- example_20/src/main/java/com/eazybytes/eazyschool/aspects/LoggerAspect.java
+++ example_20/src/main/java/com/eazybytes/eazyschool/aspects/LoggerAspect.java
@@ -8,12 +8,13 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Aspect
-//@Component
+@Component
 public class LoggerAspect {
 
     @Around("execution(* com.eazybytes.eazyschool..*.*(..))")
